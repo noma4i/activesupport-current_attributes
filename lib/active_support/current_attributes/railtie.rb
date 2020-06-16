dc1c8f17f3e567deb41a0c1cc28a2985f5c6b467
Alexander Tsirel <noma4i@gmail.com>
2020-06-17 09:57:44 +1000

    backport current_attributes for rails 4.2

--- lib/active_support/current_attributes/railtie.rb
+++ lib/active_support/current_attributes/railtie.rb
@@ -1,11 +1,29 @@
 require 'active_support'
 
+module CurrentAttributesBackport # :nodoc:
+  class Middleware # :nodoc:
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      ActiveSupport::CurrentAttributes.clear_all
+      status, headers, response = @app.call(env)
+      ActiveSupport::CurrentAttributes.clear_all
+
+      [status, headers, response]
+    end
+  end
+end
+
 module ActiveSupport
   class Railtie < Rails::Railtie # :nodoc:
     initializer "active_support.reset_all_current_attributes_instances", before: "active_support.deprecation_behavior" do |app|
-      app.reloader.before_class_unload { ActiveSupport::CurrentAttributes.clear_all }
-      app.executor.to_run              { ActiveSupport::CurrentAttributes.reset_all }
-      app.executor.to_complete         { ActiveSupport::CurrentAttributes.reset_all }
+      app.middleware.use CurrentAttributesBackport::Middleware
+
+      # app.reloader.before_class_unload { ActiveSupport::CurrentAttributes.clear_all }
+      # app.executor.to_run              { ActiveSupport::CurrentAttributes.reset_all }
+      # app.executor.to_complete         { ActiveSupport::CurrentAttributes.reset_all }
     end
   end
 end
